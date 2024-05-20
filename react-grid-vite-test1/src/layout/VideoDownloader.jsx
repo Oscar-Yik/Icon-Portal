4bde9aadcb4b6c8b73f6b061453833f3d59c6cfc
Oscar-Yik <oscar1532@outlook.com>
2024-05-20 01:55:12 -0700

    Improved Youtube Downloader Widget UI

--- react-grid-vite-test1/src/layout/VideoDownloader.jsx
+++ react-grid-vite-test1/src/layout/VideoDownloader.jsx
@@ -1,46 +1,77 @@
 import React, { useState } from 'react';
+import EditableTextItem from '../utils/EditableText';
 import DeleteIcon from '@mui/icons-material/Delete';
 import getConstants from '../utils/Constants';
 import youtubeMusicIcon from '../assets/youtube-music-icon.png';
+import conversionIcon from '../assets/conversion-icon.png';
 import failedIcon from '../assets/failed-download.png';
 
 import '../utils/Background.css'
 
 export default function VideoDownloader ({block, removeBlock, onUpdateEdit, colors}) {
     
-    const [display, setDisplay] = useState(false);
+    const [display, setDisplay] = useState(true);
     const [failed, setFailed] = useState(false);
-    const [download, setDownload] = useState(1);
-    const { defaultRowHeight } = getConstants();
-
-    function displayChoose() {
-        setDisplay(true);
-    }
+    const [download, setDownload] = useState(true);
+    const [link, setLink] = useState("Choose Youtube Link");
+    const { defaultRowHeight, colWidth, windowHeight, windowWidth } = getConstants();
 
     function loadingIcon() {
-        if (download === 1) {
+        if (download) {
             return (<div className='loading-icon'></div>)
-        } else if (download === -1) {
-            return (<img className='failed-icon'
-                         width={25}
-                         height={25}
-                         src={failedIcon}/>)
         } else {
             return null;
         }
     }
+
+    function updateLink(trash, newLink) {
+        setLink(newLink);
+    }
+
+    function downloadVideo() {
+        console.log("clicked");
+    }
+
+    // for actual download function
+    // return (<img className='failed-icon'
+    //                      width={25}
+    //                      height={25}
+    //                      src={failedIcon}/>)
 
     return (
         <div width={block.w*defaultRowHeight} 
              height={block.h*defaultRowHeight}>
             <DeleteIcon className="widget-delete" onClick={() => removeBlock(block.i)}/>
             <div className='choose-button' 
-                 onClick={() => displayChoose()}
+                 onClick={() => setDisplay(true)}
                  style={{backgroundColor: colors.headerButton, color: colors.headerFont}}>Choose Video</div>
             <img src={youtubeMusicIcon}
                  className='youtubeDown-image'/>
-            <div className='widget-header' style={{width: 308, backgroundColor: colors.header}}></div>
+            <div className='widget-header' style={{width: (4.4)*colWidth, backgroundColor: colors.header}}></div>
             {loadingIcon()}
+            {display && (
+                <div className='choose-video-box' 
+                     style={{position: 'fixed',
+                             top: '0',
+                             left: '0',
+                             width: '100%', 
+                             height:  '100%'}}
+                     onClick={() => setDisplay(false)}></div>)}
+            {display && (
+                <div className='choose-video-container'>
+                    <div className='choose-video'
+                        style={{backgroundColor: colors.headerButton}}>
+                        <EditableTextItem initialText={link} id={"youtube-link"} onStateChange={updateLink} colors={colors}/>
+                        <img className="choose-video-conversion" 
+                             src={conversionIcon}
+                             alt="Triceratops"
+                             width={20}></img>
+                        <div className='conversion-overlay'
+                             onClick={() => downloadVideo()}></div>
+                    </div>
+                    <div className='choose-video-down-triangle'
+                         style={{borderTop: `10px solid ${colors.headerButton}`}}></div>
+                </div>)}
         </div>
       );
 }
