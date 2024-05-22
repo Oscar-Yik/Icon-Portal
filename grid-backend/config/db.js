986071e0912f15a094f9177ddba9511b04b1a112
oyik <oyik>
2024-05-22 15:55:19 -0700

    Dockerized node, react, and mongodb services

--- grid-backend/config/db.js
+++ grid-backend/config/db.js
@@ -1,8 +1,9 @@
 // db.js
+require('dotenv').config({ path: __dirname + "/../../.env" });
 
 const mongoose = require("mongoose");
 const db =
-  "mongodb+srv://oscaryik228:NyxelGT3G2vNaRQ9@whatisacluster.n8fqxho.mongodb.net/?retryWrites=true&w=majority&appName=WhatIsACluster";
+  `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@whatisacluster.n8fqxho.mongodb.net/?retryWrites=true&w=majority&appName=WhatIsACluster`;
 /* Replace <password> with your database password */
 
 mongoose.set("strictQuery", true, "useNewUrlParser", true);
