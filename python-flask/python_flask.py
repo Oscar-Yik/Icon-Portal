6f34d84188526549b1b2020314c1adb9eba430e1
oyik <oyik>
2024-06-13 10:18:01 -0700

    Revert "Videos upload to s3 and paths to mongo"
    This reverts commit 8f708641d9c218e633445ff732622fabc64832d2.

--- python-flask/python_flask.py
+++ python-flask/python_flask.py
@@ -1,14 +1,8 @@
 import os
 import glob
-import boto3
-import botocore
-import botocore.exceptions
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 from flask_mysqldb import MySQL
-from pymongo import MongoClient
-from dotenv import load_dotenv
-import pymongo.errors
 from downloader import download_playlist, download_single_video
 
 app = Flask(__name__)
@@ -21,69 +15,9 @@ app = Flask(__name__)
  
 # mysql = MySQL(app)
 CORS(app)
-load_dotenv()
-
-def s3_connect():
-    try: 
-        s3_client = boto3.client(
-            service_name='s3',
-            region_name=os.getenv('AWS_REGION'),
-            aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
-            aws_secret_access_key=os.getenv('AWS_SECRET_KEY')
-        )
-        print("Successfully connected to s3")
-        return s3_client
-    except Exception as error: 
-        print("Couldn't connect to s3")
-        print(f"Error Details: {error.args}")    
-        return None
-
-def mongo_connect():
-    try: 
-        mongo_client = MongoClient(f"mongodb+srv://{os.getenv('MONGODB_USER')}:{os.getenv('MONGODB_PASSWORD')}@whatisacluster.n8fqxho.mongodb.net/{os.getenv('MONGODB_DBNAME')}?retryWrites=true&w=majority&appName=WhatIsACluster")
-        print("Successfully connected MongoDB")
-        return mongo_client
-    except pymongo.errors.ConnectionFailure as error: 
-        print("Couldn't connect to mongodb")
-        print(f"Error Details: {error.args}")    
-        return None
-
-s3_client = s3_connect()
-mongo_client = mongo_connect()
-
-def s3_upload(FILE_NAME):
-    LOCAL_FILE = './Videos/' + FILE_NAME
-    NAME_FOR_S3 = 'videos/' + FILE_NAME
-
-    if (s3_client):
-        try: 
-            s3_client.upload_file(LOCAL_FILE, os.getenv('AWS_S3_BUCKET_NAME'), NAME_FOR_S3)
-            print("Successfully uploaded file")
-            mongo_upload(FILE_NAME, NAME_FOR_S3)
-        except botocore.exceptions.ClientError as error: 
-            print("Couldn't upload video to S3")
-            print(f"Error Details: {error.args}")
-    else: 
-        print("s3 not connected")
-
-def mongo_upload(video_title, video_path): 
-    if (mongo_client):
-        database = mongo_client[os.getenv('MONGODB_DBNAME')]
-        collection = database['videos']
-        video = { "title": video_title, 
-                "video_path": video_path
-        }
-        try: 
-            collection.insert_one(video)
-            print("Successfully uploaded video path")
-        except Exception as error: 
-            print("Couldn't add video path to MongoDB")
-            print(f"Error Details: {error.args}")
-    else: 
-        print("MongoDB not connected")  
 
 # with app.app_context():
-#     # download_playlist('Piano')
+#     download_playlist('Piano')
 
 
 @app.route('/')
@@ -101,7 +35,6 @@ def postYoutube():
         return jsonify({"success": False, "message": "Missing URL in request body"}), 400
     if (request.args.get('playlist') == 'false'):
         success = download_single_video(data["url"])
-        s3_upload(success["title"][0])
     else:
         success = download_playlist(data["url"])
     return jsonify({"success": success["status"], "title": success["title"]}), 200 if success["status"] else 500
