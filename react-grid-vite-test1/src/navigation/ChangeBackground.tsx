ab7c405a94347c441c97719e54c858a6573f74cd
oyik <oyik>
2024-06-29 22:42:42 -0700

    Completely reworked background images

--- react-grid-vite-test1/src/navigation/ChangeBackground.tsx
+++ react-grid-vite-test1/src/navigation/ChangeBackground.tsx
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { ChangeEvent, useState, useEffect } from 'react';
 import "../utils/Background.css";
 import SkipNextIcon from '@mui/icons-material/SkipNext';
 import FileUploadIcon from '@mui/icons-material/FileUpload';
 import AddBoxIcon from '@mui/icons-material/AddBox';
+import SendIcon from '@mui/icons-material/Send';
+import failedIcon from '../assets/failed-download.png';
+import successIcon from '../assets/successful-download.svg';
 import EditableTextItem from '../utils/EditableText';
 import getErrorMessage from '../utils/Errors';
 
@@ -24,6 +27,8 @@ export default function ChangeBackground({ colors, display, env_HOSTNAME, backIm
 
     const [disIcons, setDisIcons] = useState<displayIcons>({ next: false, upload: false, edit: false });
     const [disImgs, setDisImgs] = useState<backImgType[]>([]);
+    const [textImage, setTextimage] = useState<string>("Double Click to Enter Image URL");
+    const [textImageIcon, setTextimageIcon] = useState<IconNumber>(1);
 
     useEffect(() => {
         setDisImgs(bkgImgs);
@@ -113,6 +118,111 @@ export default function ChangeBackground({ colors, display, env_HOSTNAME, backIm
             .catch(e => { console.log(`Didn't catch errors properly: ${getErrorMessage(e)}`) });
     }
 
+    async function readBuffer(file: File, start = 0, end = 2): Promise<Uint8Array> {
+        return new Promise((resolve, reject) => {
+          const reader = new FileReader();
+          reader.onload = () => {
+            resolve(new Uint8Array(reader.result as ArrayBuffer));
+          };
+          reader.onerror = () => {
+            reject(new Error("Error reading file"));
+          };
+          reader.readAsArrayBuffer(file.slice(start, end));
+        });
+      }
+
+    function check(headers: number[]) {
+        return (buffers: Uint8Array) =>
+            headers.every(
+                (header, index) => header === buffers[index]
+        );
+    }
+    // 89 50 4E 47 0D 0A 1A 0A
+    const isPNG = check([0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a]); 
+
+    const handleChange = async (event: ChangeEvent<HTMLInputElement>) => {
+        const input = event.target as HTMLInputElement;
+        if (input.files && input.files[0]) {
+            const file = input.files[0];
+            // const buffers = await readBuffer(file, 0, 8);
+            // const uint8Array = new Uint8Array(buffers);
+            // const realFileElement = document.querySelector("#realFileType") as HTMLElement;
+        
+            // realFileElement.innerText = `The type of ${file.name} is: ${
+            //     isPNG(uint8Array) ? "image/png" : file.type
+            // }`;
+            
+            const formData = new FormData();
+            formData.append('file', file);
+
+            // send a new request to a new endpoint with the file 
+            const response = await fetch(`http://${env_HOSTNAME}:8082/api/s3/sendImage`, { 
+                method: 'POST', 
+                body: formData
+            }); 
+            if (!response.ok) {
+                throw new Error("Couldn't upload image");
+            }
+        }
+    }
+
+    function updateTextImage(trash: any, newText: string) {
+        console.log(newText);
+        setTextimageIcon(1);
+        setTextimage(newText);
+    }
+
+    async function uploadTextImage() {
+        try {
+            const payload = { url: textImage };
+            const response = await fetch(`http://${env_HOSTNAME}:8082/api/s3/sendImageURL`, { 
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                method: 'POST', 
+                body: JSON.stringify(payload)
+            }); 
+            if (!response.ok) {
+                throw new Error("Couldn't upload image");
+            }
+            const status = await response.json(); 
+            return status; 
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    function handleTextImage() {
+        uploadTextImage()
+            .then(message => {
+                if (message.status === 0) {
+                    setTextimageIcon(0);
+                } else {
+                    setTextimageIcon(2);
+                }
+            })
+            .catch(e => {
+                console.log(getErrorMessage(e));
+                setTextimageIcon(0);
+            })
+    }
+    
+    function sendingIcon() {
+        if (textImageIcon === 2) {
+            return (<img src={successIcon}
+                alt={"Stegosaurus"}
+                width={25}
+                height={25}/>);
+        } else if (textImageIcon === 0) {
+            return (<img src={failedIcon}
+                         alt={"Stegosaurus"}
+                         width={25}
+                         height={25}/>);
+        } else {
+            return (<SendIcon style={{flex: "0.5", cursor: "pointer"}} onClick={() => handleTextImage()}/>)
+        }
+    }
+
     if (display) {
         return (
             <div>
@@ -131,21 +241,38 @@ export default function ChangeBackground({ colors, display, env_HOSTNAME, backIm
                     </div>
                     <div className='backImg-gap'></div>
                     <div className='backImg-options'>
-                        <FileUploadIcon style={{cursor: "pointer"}} onClick={() => {updateDisIcon(1); console.log(bkgImgs)}}/>
-                        <AddBoxIcon style={{cursor: "pointer"}} onClick={() => updateDisIcon(2)} />
+                        <label htmlFor="inputFile" style={{display: "block"}}>
+                            <FileUploadIcon style={{cursor: "pointer"}} onClick={() => {updateDisIcon(1); console.log(bkgImgs)}}/>
+                        </label>
+                        <input type="file" id="inputFile" accept="image/*"
+                            onChange={handleChange} />
+                        <AddBoxIcon style={{cursor: "pointer"}} onClick={() => {updateDisIcon(2); setTextimageIcon(1); } } />
                         <SkipNextIcon style={{cursor: "pointer"}} onClick={() => nextImgs()}/>
                     </div>
                     <div className='backImg-gap'></div>
                 </div>
                 {disIcons.edit && (
                     <div className='backImg-editbox' style={{backgroundColor: colors.headerButton}}>
-                        <EditableTextItem initialText={backImg.name} id={"Background"} onStateChange={updateBackImg} colors={colors}/>
+                        <div style={{display: "flex", justifyContent: "space-evenly"}}> 
+                            <div style={{flex: "6"}}>
+                                <EditableTextItem initialText={textImage} id={"Background"} 
+                                                onStateChange={(id: any, newText: string) => updateTextImage(id, newText)} 
+                                                colors={colors}/>
+                            </div>
+                            {sendingIcon()}
+                        </div>
+                        {/* <div style={{display: "flex"}}>
+                            Choose File：
+                            <label htmlFor="inputFile" style={{display: "block"}}>
+                                <FileUploadIcon style={{cursor: "pointer"}} onClick={() => {updateDisIcon(1); console.log(bkgImgs)}}/>
+                            </label>
+                            <input type="file" id="inputFile" accept="image/*"
+                                onChange={handleChange} />
+                            <p id="realFileType"></p>
+                        </div> */}
                     </div>
                 )}
             </div>
-            // <div className='background-theme' style={{backgroundColor: colors.headerButton}}>
-            //     <EditableTextItem initialText={backImg} id={"Background"} onStateChange={updateBackImg} colors={colors}/>
-            // </div>
         );
     } else {
         return null;
